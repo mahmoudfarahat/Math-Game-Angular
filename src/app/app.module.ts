864787c94cf636e41e82b6fd8fb4e451fb3d73e3
Mahmoud Farahat <mahmoud.ped@gmail.com>
2023-03-31 20:18:17 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EquationComponent } from './equation/equation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EquationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    reactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
